a6908ee1f833203a13e682bfa9c5b8611500bca3
Shreyas Biradar <119810773+ShreyasBi@users.noreply.github.com>
2023-11-20 23:36:05 +0530

    Add files via upload

---
+++ docWeb/src/data/whatsapp.js
@@ -0,0 +1,310 @@
+import {
+  cs1,
+  cs2,
+  chat1,
+  chat2,
+  chat3,
+  chat4,
+  chat5,
+  chat6,
+  chat7,
+  chat8,
+  chat9,
+  chat10,
+  chat11,
+  chat12,
+  chat13,
+} from "../assets/whatsapp";
+
+export const chatsData = [
+  {
+    id:0,
+    severity:1000,
+    pp: cs1,
+    contact: "Ava",
+    msg: "Awesome, thank you!",
+    time: "1:15 pm",
+    unreadMsgs: null,
+  },
+  {
+    id:1,
+    severity:15,
+    pp: chat1,
+    contact: "Patient-1",
+    msg: "Coding Spot is fire! 🔥",
+    time: "12:15 pm",
+    unreadMsgs: 2,
+  },
+  {
+    id:2,
+    severity:19,
+    pp: chat2,
+    contact: "Patient-2",
+    msg: "Miss you, call you later!",
+    time: "11:11 am",
+    unreadMsgs: 4,
+  },
+  {
+    id:3,
+    pp: chat3,
+    severity:4,
+    contact: "Patient-3",
+    msg: "Hey dad, need you asap!!",
+    time: "11:04 am",
+    unreadMsgs: null,
+  },
+  {
+    id:4,
+    pp: chat4,
+    severity:45,
+    contact: "Patient-4",
+    msg: "Who are you???",
+    time: "10:58 am",
+    unreadMsgs: null,
+  },
+  {
+    id:5,
+    severity:47,
+    pp: chat5,
+    contact: "Patient-5",
+    msg: "Help me with this bug!! 😭😭",
+    time: "10:50 am",
+    unreadMsgs: 23,
+  },
+  {
+    id:6,
+    pp: chat6,
+    severity:1,
+    contact: "Patient-6",
+    msg: "Call me son, love you. ❤️",
+    time: "10:35 am",
+    unreadMsgs: 2,
+  },
+  {
+    id:7,
+    pp: chat7,
+    severity:9,
+    contact: "Patient-7",
+    msg: "hahhahhaha lol 😂😂😂",
+    time: "10:18 am",
+    unreadMsgs: null,
+  },
+  {
+    id:8,
+    pp: chat8,
+    severity:29,
+    contact: "Patient-8",
+    msg: "Meeting in 2 hours.",
+    time: "10:02 am",
+    unreadMsgs: null,
+  },
+  {
+    id:9,
+    pp: chat9,
+    severity:24,
+    contact: "Patient-9",
+    msg: "The test was sooo hard 😓",
+    time: "9:47 am",
+    unreadMsgs: 3,
+  },
+];
+
+
+
+export const messagesData = [
+  [
+    // Conversation 1
+    {
+      msg: "Hi Doctor! i am here to assist you!!",
+      time: "9:00 am",
+      sent: false,
+    },
+    {
+      msg: "x patients are required to be consulted",
+      time: "9:10 am",
+      sent: false,
+    },
+    {
+      msg:"Patients are sorted according to there severity!!!",
+      time: "9:10 am",
+      sent: false,
+    },
+    {
+      msg:"Press yes to do so",
+      time: "9:10 am",
+      sent: false,
+    }
+  ],
+  [
+    // Conversation 2
+    {
+      msg: "Hello, Doctor. I've had a persistent cough and fever for a few days.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      msg: "I've been experiencing persistent headaches, especially in the mornings. Recently, I've noticed a tingling sensation in my hands and feet, and sometimes I have difficulty maintaining my balance. My vision seems to be blurry at times, and I've had moments of forgetfulness. Additionally, there are instances when my speech feels slurred. These symptoms have been ongoing for the past few weeks.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      "msg": "Possible diseases associated with these symptoms:\n\nMigraine: Headaches, visual disturbances, and sometimes tingling sensations.\nMultiple Sclerosis (MS): Tingling, balance issues, blurred vision, and cognitive difficulties.\nStroke: Sudden onset of slurred speech, balance problems, and cognitive issues.\nBrain Tumor: Persistent headaches, balance problems, and cognitive changes.\nPeripheral Neuropathy: Tingling in the hands and feet.",
+      "time": "11:00 am",
+      "sent": false
+    }
+    
+  ],
+  [
+    // Conversation 2
+    {
+      msg: "Hello, Doctor. I've had a persistent cough and fever for a few days.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      msg: "I've been experiencing persistent headaches, especially in the mornings. Recently, I've noticed a tingling sensation in my hands and feet, and sometimes I have difficulty maintaining my balance. My vision seems to be blurry at times, and I've had moments of forgetfulness. Additionally, there are instances when my speech feels slurred. These symptoms have been ongoing for the past few weeks.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      "msg": "Possible diseases associated with these symptoms:\n\nMigraine: Headaches, visual disturbances, and sometimes tingling sensations.\nMultiple Sclerosis (MS): Tingling, balance issues, blurred vision, and cognitive difficulties.\nStroke: Sudden onset of slurred speech, balance problems, and cognitive issues.\nBrain Tumor: Persistent headaches, balance problems, and cognitive changes.\nPeripheral Neuropathy: Tingling in the hands and feet.",
+      "time": "11:00 am",
+      "sent": false
+    }
+    
+  ],
+  [
+    // Conversation 2
+    {
+      msg: "Hello, Doctor. I've had a persistent cough and fever for a few days.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      msg: "I've been experiencing persistent headaches, especially in the mornings. Recently, I've noticed a tingling sensation in my hands and feet, and sometimes I have difficulty maintaining my balance. My vision seems to be blurry at times, and I've had moments of forgetfulness. Additionally, there are instances when my speech feels slurred. These symptoms have been ongoing for the past few weeks.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      "msg": "Possible diseases associated with these symptoms:\n\nMigraine: Headaches, visual disturbances, and sometimes tingling sensations.\nMultiple Sclerosis (MS): Tingling, balance issues, blurred vision, and cognitive difficulties.\nStroke: Sudden onset of slurred speech, balance problems, and cognitive issues.\nBrain Tumor: Persistent headaches, balance problems, and cognitive changes.\nPeripheral Neuropathy: Tingling in the hands and feet.",
+      "time": "11:00 am",
+      "sent": false
+    }
+    
+  ],
+  [
+    // Conversation 2
+    {
+      msg: "Hello, Doctor. I've had a persistent cough and fever for a few days.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      msg: "I've been experiencing persistent headaches, especially in the mornings. Recently, I've noticed a tingling sensation in my hands and feet, and sometimes I have difficulty maintaining my balance. My vision seems to be blurry at times, and I've had moments of forgetfulness. Additionally, there are instances when my speech feels slurred. These symptoms have been ongoing for the past few weeks.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      "msg": "Possible diseases associated with these symptoms:\n\nMigraine: Headaches, visual disturbances, and sometimes tingling sensations.\nMultiple Sclerosis (MS): Tingling, balance issues, blurred vision, and cognitive difficulties.\nStroke: Sudden onset of slurred speech, balance problems, and cognitive issues.\nBrain Tumor: Persistent headaches, balance problems, and cognitive changes.\nPeripheral Neuropathy: Tingling in the hands and feet.",
+      "time": "11:00 am",
+      "sent": false
+    }
+    
+  ],
+  [
+    // Conversation 2
+    {
+      msg: "Hello, Doctor. I've had a persistent cough and fever for a few days.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      msg: "I've been experiencing persistent headaches, especially in the mornings. Recently, I've noticed a tingling sensation in my hands and feet, and sometimes I have difficulty maintaining my balance. My vision seems to be blurry at times, and I've had moments of forgetfulness. Additionally, there are instances when my speech feels slurred. These symptoms have been ongoing for the past few weeks.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      "msg": "Possible diseases associated with these symptoms:\n\nMigraine: Headaches, visual disturbances, and sometimes tingling sensations.\nMultiple Sclerosis (MS): Tingling, balance issues, blurred vision, and cognitive difficulties.\nStroke: Sudden onset of slurred speech, balance problems, and cognitive issues.\nBrain Tumor: Persistent headaches, balance problems, and cognitive changes.\nPeripheral Neuropathy: Tingling in the hands and feet.",
+      "time": "11:00 am",
+      "sent": false
+    }
+    
+  ],
+  [
+    // Conversation 2
+    {
+      msg: "Hello, Doctor. I've had a persistent cough and fever for a few days.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      msg: "I've been experiencing persistent headaches, especially in the mornings. Recently, I've noticed a tingling sensation in my hands and feet, and sometimes I have difficulty maintaining my balance. My vision seems to be blurry at times, and I've had moments of forgetfulness. Additionally, there are instances when my speech feels slurred. These symptoms have been ongoing for the past few weeks.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      "msg": "Possible diseases associated with these symptoms:\n\nMigraine: Headaches, visual disturbances, and sometimes tingling sensations.\nMultiple Sclerosis (MS): Tingling, balance issues, blurred vision, and cognitive difficulties.\nStroke: Sudden onset of slurred speech, balance problems, and cognitive issues.\nBrain Tumor: Persistent headaches, balance problems, and cognitive changes.\nPeripheral Neuropathy: Tingling in the hands and feet.",
+      "time": "11:00 am",
+      "sent": false
+    }
+    
+  ],
+  [
+    // Conversation 2
+    {
+      msg: "Hello, Doctor. I've had a persistent cough and fever for a few days.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      msg: "I've been experiencing persistent headaches, especially in the mornings. Recently, I've noticed a tingling sensation in my hands and feet, and sometimes I have difficulty maintaining my balance. My vision seems to be blurry at times, and I've had moments of forgetfulness. Additionally, there are instances when my speech feels slurred. These symptoms have been ongoing for the past few weeks.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      "msg": "Possible diseases associated with these symptoms:\n\nMigraine: Headaches, visual disturbances, and sometimes tingling sensations.\nMultiple Sclerosis (MS): Tingling, balance issues, blurred vision, and cognitive difficulties.\nStroke: Sudden onset of slurred speech, balance problems, and cognitive issues.\nBrain Tumor: Persistent headaches, balance problems, and cognitive changes.\nPeripheral Neuropathy: Tingling in the hands and feet.",
+      "time": "11:00 am",
+      "sent": false
+    }
+    
+  ],
+  [
+    // Conversation 2
+    {
+      msg: "Hello, Doctor. I've had a persistent cough and fever for a few days.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      msg: "I've been experiencing persistent headaches, especially in the mornings. Recently, I've noticed a tingling sensation in my hands and feet, and sometimes I have difficulty maintaining my balance. My vision seems to be blurry at times, and I've had moments of forgetfulness. Additionally, there are instances when my speech feels slurred. These symptoms have been ongoing for the past few weeks.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      "msg": "Possible diseases associated with these symptoms:\n\nMigraine: Headaches, visual disturbances, and sometimes tingling sensations.\nMultiple Sclerosis (MS): Tingling, balance issues, blurred vision, and cognitive difficulties.\nStroke: Sudden onset of slurred speech, balance problems, and cognitive issues.\nBrain Tumor: Persistent headaches, balance problems, and cognitive changes.\nPeripheral Neuropathy: Tingling in the hands and feet.",
+      "time": "11:00 am",
+      "sent": false
+    }
+    
+  ],
+  [
+    // Conversation 2
+    {
+      msg: "Hello, Doctor. I've had a persistent cough and fever for a few days.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      msg: "I've been experiencing persistent headaches, especially in the mornings. Recently, I've noticed a tingling sensation in my hands and feet, and sometimes I have difficulty maintaining my balance. My vision seems to be blurry at times, and I've had moments of forgetfulness. Additionally, there are instances when my speech feels slurred. These symptoms have been ongoing for the past few weeks.",
+      time: "11:00 am",
+      sent: false,
+    },
+    {
+      "msg": "Possible diseases associated with these symptoms:\n\nMigraine: Headaches, visual disturbances, and sometimes tingling sensations.\nMultiple Sclerosis (MS): Tingling, balance issues, blurred vision, and cognitive difficulties.\nStroke: Sudden onset of slurred speech, balance problems, and cognitive issues.\nBrain Tumor: Persistent headaches, balance problems, and cognitive changes.\nPeripheral Neuropathy: Tingling in the hands and feet.",
+      "time": "11:00 am",
+      "sent": false
+    }
+    
+  ],
+  // Add more conversations here if needed
+];
